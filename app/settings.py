247cda22de62eb62b8b0ab2fa0ded0adc73b114d
gukubiack <kubiack.dg@gmail.com>
2024-08-28 20:02:14 -0300

    Add static_files in settings

--- app/settings.py
+++ app/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_FILES = os.path.join(BASE_DIR, "staticfiles") 
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
